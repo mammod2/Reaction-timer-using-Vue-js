8f5d864f1405869c246b2b79c53ce1332758035b
mammod2 <mammod26@gmail.com>
2021-09-21 23:19:00 +0800

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Hamudi's Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <block v-if="isPlaying" :delay="delay" @end="endGame" />
+
+  <results v-if="showResults" :score="score" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import block from "./components/Block.vue";
+import Results from "./components/Results.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { block, Results },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false,
+    };
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+      this.showResults = false;
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.isPlaying = false;
+      this.showResults = true;
+    },
+  },
+};
 </script>
 
 <style>
@@ -20,7 +41,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #0e1114;
   margin-top: 60px;
 }
 </style>
